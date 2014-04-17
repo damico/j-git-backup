73aba9f6c8f8bc48a049c16a40c5802c8c29d5ab
root <root@hit-nxdomain.opendns.com>
2014-04-17 12:07:53 -0300

    added/fixed gpl information at all classes

--- src/org/jdamico/jgitbkp/commons/JGitBackupException.java
+++ src/org/jdamico/jgitbkp/commons/JGitBackupException.java
@@ -1,19 +1,19 @@
 package org.jdamico.jgitbkp.commons;
 
 /*
- * This file is part of SECNOTE (written by Jose Damico).
+ * This file is part of J-GIT-BACKUP (written by Jose Damico).
  * 
- *    SECNOTE is free software: you can redistribute it and/or modify
+ *    J-GIT-BACKUP is free software: you can redistribute it and/or modify
  *    it under the terms of the GNU General Public License (version 2) 
  *    as published by the Free Software Foundation.
  *
- *    SECNOTE is distributed in the hope that it will be useful,
+ *    J-GIT-BACKUP is distributed in the hope that it will be useful,
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
  *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *    GNU General Public License for more details.
  *
  *    You should have received a copy of the GNU General Public License
- *    along with SECNOTE.  If not, see <http://www.gnu.org/licenses/>.
+ *    along with J-GIT-BACKUP.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 public class JGitBackupException extends Exception {
--- src/org/jdamico/jgitbkp/commons/LoggerManager.java
+++ src/org/jdamico/jgitbkp/commons/LoggerManager.java
@@ -1,5 +1,21 @@
 package org.jdamico.jgitbkp.commons;
 
+/*
+ * This file is part of J-GIT-BACKUP (written by Jose Damico).
+ * 
+ *    J-GIT-BACKUP is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License (version 2) 
+ *    as published by the Free Software Foundation.
+ *
+ *    J-GIT-BACKUP is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with J-GIT-BACKUP.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
